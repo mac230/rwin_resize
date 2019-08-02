189dcc0c66c0288814d78c09617ff7a3d982e107
Mahlon Collins <mahlon@umn.edu>
2019-08-02 17:32:55 -0500

    Fix 'mac_list_sublist_functions.el' and incorporate todo functions.

--- mac_list_sublist_functions.el
+++ mac_list_sublist_functions.el
@@ -3,8 +3,11 @@
 (defvar non-blank-line-regex "\\(^[^[:space:]\n]+\\)"
   "regex for a non-blank line; used to demarcate list boundaries")
 
-(defvar list-regex "^ + - \\[[[:digit:]]+\\] \\(.*:: \\)\\{0,1\\}"
+(defvar list-regex "^ + - \\[[[:digit:]x]*\\] \\(.*:: \\)\\{0,1\\}"
   "regex for a list element; used to mark where sublists should go")
+
+(defvar todo-regex "^ + - \\[[x]*\\]\\(.*:: \\)\\{0,1\\}"
+  "regex for a todo list element; used to mark where sublists should go")
 
 (defvar sublist-column-regex "^ +\\+"
   "regex used to determine the column to insert the next '+' char")
@@ -16,15 +19,13 @@
 (defun list-line-adder ()
   "Add newlines between lines of my lists where needed."
   (interactive)
-  (let* ((list-elt-regex "\\(^    - \\[[[:digit:]]+\\]\\).*\n\\(^ +\\+.*\n\\)*\\(^ .*\n\\)*")
-      (non-blank-regex "\\(^[^[:space:]\n]+\\)")
-       (reverse-bound
-        (save-excursion
-          (re-search-backward non-blank-regex nil t)))
-       (forward-bound
-        (save-excursion
-          (re-search-forward non-blank-regex nil t)))
-       (next-elt))
+  (let* ((reverse-bound
+          (save-excursion
+            (re-search-backward non-blank-line-regex nil t)))
+         (forward-bound
+          (save-excursion
+            (re-search-forward non-blank-line-regex nil t)))
+         (next-elt))
  
   (when
       (not reverse-bound)
@@ -35,16 +36,16 @@
     (setq forward-bound (point-max))) 
 
   (goto-char forward-bound)
-  (setq next-elt (save-excursion (re-search-backward list-elt-regex reverse-bound t 2)))
+  (setq next-elt (save-excursion (re-search-backward list-regex reverse-bound t 2)))
 
   ;; insert needed newlines
   (while (and next-elt
               (>= (point) reverse-bound))
     (progn
-      (re-search-backward list-elt-regex next-elt t)
+      (re-search-backward list-regex next-elt t)
       (when (not (re-search-backward "^\n" next-elt t))
         (open-line 1))
-      (setq next-elt (save-excursion (re-search-backward list-elt-regex reverse-bound t 2)))
+      (setq next-elt (save-excursion (re-search-backward list-regex reverse-bound t 2)))
       ))
   ))
 
@@ -54,15 +55,14 @@
 (defun list-line-remover ()
   "Remove extraneous newlines from my lists." 
   (interactive)
-  (let* ((list-elt-regex "\\(^    - \\[[[:digit:]]+\\]\\).*\n\\(^ +\\+.*\n\\)*\\(^ .*\n\\)*")
-      (non-blank-regex "\\(^[^[:space:]\n]+\\)")
-       (reverse-bound
-        (save-excursion
-          (re-search-backward non-blank-regex nil t)))
-       (forward-bound
-        (save-excursion
-          (re-search-forward non-blank-regex nil t)))
-       (next-elt))
+  (let* ((list-elt-regex "\\(^    - \\[[[:digit:]x]*\\]\\).*\n\\(^ +\\+.*\n\\)*\\(^ .*\n\\)*")
+         (reverse-bound
+          (save-excursion
+            (re-search-backward non-blank-line-regex nil t)))
+         (forward-bound
+          (save-excursion
+            (re-search-forward non-blank-line-regex nil t)))
+         (next-elt))
  
   (when
       (not reverse-bound)
@@ -87,67 +87,20 @@
   ))
 
 
-(defun list-renumber ()
-  "Automatically re-number lists and return point to its present location."
-  (interactive)
-  (let* ((current-point (point))
-         (list-n 1)
-         (non-blank "\\(^[^[:space:]\n[:digit:]]+\\)")
-         (reverse-bound
-          (save-excursion
-            (re-search-backward non-blank nil t)))
-         (forward-bound
-          (save-excursion
-            (re-search-forward non-blank nil t))))
-
-    (when
-        (not reverse-bound)
-      (setq reverse-bound (point-min)))
-
-    (when
-        (not forward-bound)
-      (setq forward-bound (point-max))) 
-    
-    (goto-char reverse-bound)
-    (unless (eobp)
-      (forward-char 1))
-
-    (while
-        (<= (point) (1- forward-bound))
-
-    (if
-      (re-search-forward "\\(^[^+\n]*?\\)\\([[:digit:]]+\\)" forward-bound t)
-
-        (progn
-          (goto-char (match-beginning 1))
-          (re-search-forward "[[:digit:]]+" nil t)
-          (replace-match (format "%s" list-n))
-          (end-of-line)
-          (setq list-n (1+ list-n)))
-      
-      (if (eobp)
-          (progn
-            (end-of-line)
-            (insert "\n"))
-        (forward-char 1)))
-    )
-
-    (goto-char current-point)
-  ))
 
 
+;; -----
 (defun list-line-renumber ()
   "Automatically re-number lists and return point to its present location."
   (interactive)
   (let* ((current-point (point))
          (list-n 1)
-         (non-blank "\\(^[^ \n[:digit:]]+\\)")
          (reverse-bound
           (save-excursion
-            (re-search-backward non-blank nil t)))
+            (re-search-backward non-blank-line-regex nil t)))
          (forward-bound
           (save-excursion
-            (re-search-forward non-blank nil t))))
+            (re-search-forward non-blank-line-regex nil t))))
 
     (when
         (not reverse-bound)
@@ -186,7 +139,9 @@
 
 
 
-(defun line-single-spacer ()
+
+;; -----
+(defun list-single-spacer ()
   "Create a single-spaced list."
   (interactive)
   (let ((reverse-bound
@@ -219,26 +174,27 @@
       )))
 
 
+
+
+;; -----
 (defun mac-org-numbered-list ()
 "Function to insert numbered lists in my preferred format of \'- [1] first point.\'"
 (interactive)
 (end-of-line)
 (let* ((current-point (point))
        (current-number 1)
-       (list-elt-regex "\\(^    - \\[[[:digit:]]+\\]\\).*\n\\(^ +\\+.*\n\\)*\\(^ .*\n\\)*")
-       (non-blank-regex "\\(^[^[:space:]\n]+\\)")
        (reverse-bound
         (save-excursion
-          (re-search-backward non-blank-regex nil t)))
+          (re-search-backward non-blank-line-regex nil t)))
        (forward-bound
         (save-excursion
-          (re-search-forward non-blank-regex nil t)))
+          (re-search-forward non-blank-line-regex nil t)))
        (next-list-elt
         (save-excursion
-          (re-search-forward "^    - \\[[[:digit:]]+\\]" forward-bound t)))
+          (re-search-forward list-regex forward-bound t)))
        (prev-list-elt
         (save-excursion
-          (re-search-backward list-elt-regex reverse-bound t))))
+          (re-search-backward list-regex reverse-bound t))))
       
   ;; contingency for when you start a list at the beginning/end of a buffer
   (when
@@ -283,6 +239,96 @@
   (end-of-line)
   (message current-number)
   ))
+
+
+
+
+
+;; -----
+(defun mac-org-todo-list ()
+"Insert to-do lists in my preferred format."
+(interactive)
+(end-of-line)
+(let* ((current-point (point))
+       (reverse-bound
+        (save-excursion
+          (re-search-backward non-blank-line-regex nil t)))
+       (forward-bound
+        (save-excursion
+          (re-search-forward non-blank-line-regex nil t)))
+       (next-list-elt
+        (save-excursion
+          (re-search-forward todo-regex forward-bound t)))
+       (prev-list-elt
+        (save-excursion
+          (re-search-backward todo-regex reverse-bound t))))
+
+  ;; contingency for when you start a list at the beginning/end of a buffer
+  (when
+      (not reverse-bound)
+    (setq reverse-bound (point-min)))
+
+  (when
+      (not forward-bound)
+      (setq forward-bound (point-max)))
+
+  (when next-list-elt
+    (progn
+      (goto-char next-list-elt)
+      (beginning-of-line)
+      (open-line 1)))
+
+  (when (not next-list-elt)
+    ;; regex to get through sublists
+    (re-search-forward "\\(^ +\\+.*\n\\)+\\(^ .*\n\\)*" forward-bound t))
+
+  (insert (concat "\n    - []~~ \n"))
+  (list-line-adder)
+  (list-line-remover)
+  (end-of-buffer)
+  (re-search-backward "~~" reverse-bound t)
+  (replace-match " ")
+  ))
+
+
+
+
+
+;; -----
+(defun mac-org-numbered-or-todo ()
+  "Insert a todo or numbered list item depending on context."
+  (interactive)
+  (let* ((current-point (point))
+         (reverse-bound
+          (save-excursion
+            (re-search-backward non-blank-line-regex nil t)))
+         (forward-bound
+          (save-excursion
+            (re-search-forward non-blank-line-regex nil t)))
+         (next-list-elt
+          (save-excursion
+            (re-search-forward todo-regex forward-bound t)))
+         (prev-list-elt
+          (save-excursion
+            (re-search-backward todo-regex reverse-bound t)))) 
+
+    (when
+        (not reverse-bound)
+      (setq reverse-bound (point-min)))
+    (when
+        (not forward-bound)
+      (setq forward-bound (point-max)))
+
+    (cond
+     ((or
+       (save-excursion
+         (re-search-forward todo-regex forward-bound t))
+       (save-excursion
+         (re-search-backward todo-regex reverse-bound t)))
+      (mac-org-todo-list))
+
+     (t
+      (mac-org-numbered-list)))))
 
 
 
@@ -357,14 +403,24 @@ For when calling at the end of a wrapped text sublist line w/ sublists."
   (let ((cp (point))
         (forward-bound
          (save-excursion
-           (re-search-forward list-regex nil t))))
+           (re-search-forward list-regex nil t)))
+        (reverse-bound
+         (save-excursion
+           (re-search-backward list-regex nil t))))
 
     (when (not forward-bound)
-      (setq forward-bound (save-excursion
-                            (re-search-forward non-blank-line-regex nil t))))
-    
+      (setq forward-bound
+            (save-excursion
+              (re-search-forward non-blank-line-regex nil t))))
     (when (not forward-bound)
       (setq forward-bound (point-max)))
+
+    (when (not reverse-bound)
+      (setq reverse-bound
+            (save-excursion
+              (re-search-backward non-blank-line-regex nil t))))
+    (when (not reverse-bound)
+      (setq reverse-bound (point-max)))
 
   (let ((curr-col)
         (next-sublist)
@@ -379,17 +435,30 @@ For when calling at the end of a wrapped text sublist line w/ sublists."
                              (re-search-forward "\n +\\+.*$" forward-bound t)
                              (point)))))
 
-      (if next-sublist
-          (progn
-            (setq curr-col
-                  (save-excursion
-                    (re-search-forward sublist-column-regex next-sublist)
-                    (1- (current-column))))
-            (re-search-forward "\n +\\+.*$" forward-bound t)
-            (goto-char (match-beginning 0))
-            (insert (concat "\n" (make-string curr-col #x20) "+ ")))
-        (goto-char cp)
-        ))))
+    (cond
+     (next-sublist
+      (progn
+        (setq curr-col
+              (save-excursion
+                (re-search-forward sublist-column-regex next-sublist)
+                (1- (current-column))))
+        (re-search-forward "\n +\\+.*$" forward-bound t)
+        (goto-char (match-beginning 0))
+        (insert (concat "\n" (make-string curr-col #x20) "+ "))))
+     
+     ((looking-at (concat "\n" (substring list-regex 1)))
+      (progn
+        (setq curr-col
+              (save-excursion
+                (re-search-backward sublist-column-regex reverse-bound t)
+                (goto-char (match-end 0))
+                (1- (current-column))))
+        (unless (not curr-col)
+          (insert (concat "\n" (make-string curr-col #x20) "+ ")))))
+     
+     (t
+      (goto-char cp)))
+        )))
 
 
 
@@ -486,5 +555,5 @@ how to insert the sublist."
    )
   )
 
-(global-set-key (kbd "C-c M-RET") 'mac-org-numbered-list)
+(global-set-key (kbd "C-c M-RET") 'mac-org-numbered-or-todo)
 (global-set-key (kbd "C-c C-j")   'mac-sublist-indent)
